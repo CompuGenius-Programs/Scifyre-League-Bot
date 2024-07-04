1f26a2b9277b914f6d9742cd68e2e3ca3583908c
CompuGenius <contact@cgprograms.com>
2024-07-03 20:58:51 -0400

    Migrated to pycord and removed unnecessary code

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 import os
 
 import discord
-import dotenv
-import youtube_dl
 import emoji
+import youtube_dl
+from dotenv import load_dotenv
 
-from discord.ext import commands
-from discord_slash import SlashCommand
-from discord_slash import SlashContext
-
-dotenv.load_dotenv()
-token = os.getenv("TOKEN")
-
-songs = {
-    ":world_map:": {
-        "title": "Main Menu",
-        "url": "https://youtu.be/rpuwv7NCY4k",
-    }, ":crossed_swords:": {
-        "title": "Battle",
-        "url": "https://youtu.be/1rSUljWH6fY",
-    }
-}
+load_dotenv()
+bot = discord.Bot(intents=discord.Intents.all())
 
 chat_channel = 698961123097051181
 music_channel = 776824386950397992
@@ -34,27 +20,14 @@ music_messages = []
 header_messages = []
 playing_messages = []
 
-# Suppress noise about console usage from errors
+songs = {}
+
+ffmpeg_options = {'options': '-vn'}
 youtube_dl.utils.bug_reports_message = lambda: ''
-
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
-}
-
-ffmpeg_options = {
-    'options': '-vn'
-}
-
+ytdl_format_options = {'format': 'bestaudio/best', 'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+                       'restrictfilenames': True, 'noplaylist': True, 'nocheckcertificate': True, 'ignoreerrors': False,
+                       'logtostderr': False, 'quiet': True, 'no_warnings': True, 'default_search': 'auto',
+                       'source_address': '0.0.0.0'}
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 
@@ -72,30 +45,13 @@ class YTDLSource(discord.PCMVolumeTransformer):
         data = await bot.loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=True))
 
         if 'entries' in data:
-            # take first item from a playlist
             data = data['entries'][0]
 
         filename = ytdl.prepare_filename(data)
         return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
-intents = discord.Intents.default()
-intents.members = True
-
-bot = commands.Bot(command_prefix='-', intents=discord.Intents.all())
-bot.remove_command('help')
-slash = SlashCommand(bot, auto_register=True, auto_delete=True)
-
-
-def create_embed(title, description, color, footer, image="", *, url="", author="", author_url=""):
-    embed = discord.Embed(title=title, description=description, url=url, color=color)
-    embed.set_footer(text=footer)
-    embed.set_thumbnail(url=image)
-    embed.set_author(name=author, url=author_url)
-    return embed
-
-
-async def play(react_msg, emoji_name, user, voice_client):
+async def play(react_msg, emoji_name, voice_client):
     if voice_client is None:
         await bot.get_channel(music_channel).connect()
 
@@ -103,8 +59,7 @@ async def play(react_msg, emoji_name, user, voice_client):
 
     async with bot.get_channel(bot_channel).typing():
         player = await YTDLSource.from_url(url)
-        voice_client.play(player,
-                          after=lambda e: bot.loop.create_task(handle_done_playing(react_msg, voice_client, user)))
+        voice_client.play(player, after=lambda e: bot.loop.create_task(handle_done_playing(react_msg, voice_client)))
 
     msg = await bot.get_channel(bot_channel).send("**Playing now:** *%s*" % url)
     playing_messages.append(msg)
@@ -123,7 +78,7 @@ async def prepare_player(member):
     %s
     ''' % '\n'.join(display_titles)
 
-    embed = create_embed(title="Starfire Soundtracks", description=description, color=discord.Color.green(),
+    embed = create_embed(title="Scifyre League Soundtracks", description=description, color=discord.Color.green(),
                          footer="All soundtracks are under the copyright of CompuGenius Programs.",
                          url="https://www.youtube.com/channel/UCW7RfG26VQTchAmw_fgMV9g")
 
@@ -138,7 +93,7 @@ async def prepare_player(member):
     music_messages.append(msg)
 
 
-async def handle_done_playing(react_msg, voice_client, member):
+async def handle_done_playing(react_msg, voice_client):
     if voice_client.is_connected():
         for message in playing_messages:
             await message.delete()
@@ -149,50 +104,6 @@ async def handle_done_playing(react_msg, voice_client, member):
         for song in songs:
             await react_msg.add_reaction(emoji.emojize(song))
         await react_msg.add_reaction(emoji.emojize(":cross_mark:"))
-
-
-@slash.slash(name="help", description="Get help for using the bot.")
-async def _help(ctx):
-    description = '''
-    A bot created by <@496392770374860811> for the Starfire server.
-
-    • Join the 🎶-Music voice chat to listen to Starfire soundtracks
-    
-    /links | Lists important links
-    /help | Displays this message
-    '''
-
-    embed = create_embed(title="Starfire Bot Help", description=description, color=discord.Color.green(),
-                         image="https://starfire.cgprograms.com/images/logo.png", url="https://starfire.cgprograms.com",
-                         footer="© CompuGenius Programs. All rights reserved.")
-
-    await bot.get_channel(bot_channel).send(embed=embed)
-
-
-@slash.slash(name="links", description="List important Starfire links.")
-async def links(ctx):
-    description = '''
-    **Website:** *<https://starfire.cgprograms.com>*
-    **Discord:** *<https://discord.gg/F9xykMJ>*
-    '''
-
-    embed = create_embed(title="Important Starfire Links", description=description,
-                         color=discord.Color.purple(), footer="© CompuGenius Programs. All rights reserved.",
-                         image="https://starfire.cgprograms.com/images/logo.png",
-                         author="Starfire", author_url="https://starfire.cgprograms.com")
-
-    await bot.get_channel(bot_channel).send(embed=embed)
-
-
-@bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
-
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing,
-                                                        name="Managing the Starfire server. Type /help."))
 
 
 @bot.event
@@ -218,9 +129,9 @@ async def on_reaction_add(reaction, user):
         if react_msg in music_messages:
             voice_client = discord.utils.get(bot.voice_clients, guild=bot.get_guild(server))
             if emoji_name in songs:
-                await play(react_msg, emoji_name, user, voice_client)
+                await play(react_msg, emoji_name, voice_client)
             elif emoji_name == ":cross_mark:":
-                await voice_client.disconnect()
+                await voice_client.disconnect(force=True)
                 await user.move_to(None)
                 for message in header_messages + playing_messages:
                     await message.delete()
@@ -231,7 +142,7 @@ async def on_reaction_add(reaction, user):
 
 
 @bot.event
-async def on_voice_state_update(member, before, after):
+async def on_voice_state_update(member, _, after):
     music_voice_channel = bot.get_channel(music_channel)
     voice_client = discord.utils.get(bot.voice_clients, guild=bot.get_guild(server))
     if not member.bot:
@@ -243,7 +154,7 @@ async def on_voice_state_update(member, before, after):
 
     if len(music_voice_channel.members) <= 1:
         if voice_client and voice_client.is_connected():
-            await voice_client.disconnect()
+            await voice_client.disconnect(force=True)
             for message in header_messages + music_messages + playing_messages:
                 await message.delete()
             playing_messages.clear()
@@ -251,4 +162,23 @@ async def on_voice_state_update(member, before, after):
             music_messages.clear()
 
 
-bot.run(token)
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="Scifyre League"))
+
+
+def create_embed(title, description, color, footer, image="", *, url="", author="", author_url=""):
+    embed = discord.Embed(title=title, description=description, url=url, color=color)
+    embed.set_footer(text=footer)
+    embed.set_thumbnail(url=image)
+    embed.set_author(name=author, url=author_url)
+    return embed
+
+
+token = os.getenv("TOKEN")
+bot.run()
